af4a5e01e8bc3fc0d71ba9cc472352eea1b3bd3d
UralNabiev <devuralnabiev@gmail.com>
2024-02-26 15:19:48 +0500

    Initial Commit

---
+++ CarAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarAnimation
+//
+//  Created by O'ral Nabiyev on 26/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
